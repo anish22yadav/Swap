0db3a94baef8a3a5c97b703f7401900c9b5c65d2
anish22yadav <anishkumaryadav964@gmail.com>
2025-01-30 11:59:53 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+       a=b;
+       b=c;
+        System.out.println("after swapping the value of a is "+a);
+        System.out.println("after swapping the value of b is "+c);
+
+    }
+}
